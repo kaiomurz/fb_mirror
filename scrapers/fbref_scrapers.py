da03cceeea9c0c1c714f54bbe49630a09e218a0d
Kaiomurz Motawara <kaiomurz@gmail.com>
2022-01-24 19:58:22 +0000

    create and debug PlayerURLsScraper

--- scrapers/fbref_scrapers.py
+++ scrapers/fbref_scrapers.py
@@ -1,6 +1,7 @@
 from lxml import etree
 import pandas as pd
 
+
 from scrapers import abstract_scraper as a
 
 
@@ -8,12 +9,7 @@ class ClubURLsScraper(a.AbstractScraper):
     def __init__(self) -> None:
         print("Club URLs scraper")
         self.urls = ['https://fbref.com/en/comps/Big5/Big-5-European-Leagues-Stats']
-        self.html = None
-        self.soup = None
-        self.result = None
-        self.max_workers = 1
-        self.counter = 0
-    
+        self.max_workers = 1            
         
     def extract_data(self):
         print("extract data")        
@@ -25,28 +21,24 @@ class ClubURLsScraper(a.AbstractScraper):
 
 class PlayerURLsScraper(a.AbstractScraper):
     def __init__(self) -> None:
-        print("Player URLs scraper")
-        self.urls = []
-        self.html = None
-        self.soup = None
-        self.result = None
+        print("Player URLs scraper")        
         self.max_workers = 1
-        self.counter = 0
-
-    def run(self):
-        #validate whether there's a URL        
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor: 
-            executor.map(self.crawl, self.urls)
-
-    def crawl(self, url):
-        print("in crawl", url)
-        self.html = requests.get(url).text
-        self.soup = BeautifulSoup(self.html, 'html.parser')
-        self.extract_data()
+        self.result = {}
+        self.counter = 0    
         
     def extract_data(self):
-        print("extract data")        
+        print("in extract")
         table = self.soup.find('tbody')
         links = table.find_all('a')
-        self.result = ["https://fbref.com" + link['href'] for link in links if 'squads' in link['href']]
+        player_urls = ["https://fbref.com" + link['href']\
+        for link in links if\
+        'players' in link['href'] and\
+        'matchlogs' not in link['href']]
+        # self.result += player_urls
+        # print("after extraction", player_urls)
+        for url in player_urls:### slicing for testing only. drop eventually.
+            print("in loop", url)
+            self.counter += 1
+            self.result[self.counter] = url
+            # self.result[self.counter] = url
 
