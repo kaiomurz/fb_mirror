33c35698572a30feec9a63e4635decf5ae11c773
Kaiomurz Motawara <kaiomurz@gmail.com>
2022-01-24 11:41:16 +0000

    create ClubURLScraper as subclass of AbstractScraper

---
+++ scrapers/abstract_scraper.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class AbstractScraper(ABC):
+    def __init__(self) -> None:
+        print("abstract scraper")
+        self.urls = []
+        self.html = None
+        self.soup = None
+        self.result = None
+        self.max_workers = 3
+
+    @abstractmethod
+    def run_crawler(self):
+        pass
+
+    @abstractmethod
+    def start_crawl_threads(self, urls):        
+        pass
+
+    @abstractmethod    
+    def get_soup(self, url):
+        pass
+
+    @abstractmethod        
+    def extract_data(self, soup):
+        pass
+
+    @abstractmethod    
+    def show_result(self, result):
+        pass
+
+    @abstractmethod    
+    def save_result(self,result):
+        pass
+
+
---
+++ scrapers/fbref_scrapers.py
@@ -0,0 +1,44 @@
+
+from abstract_scraper import AbstractScraper
+import threading
+import concurrent.futures
+import re
+import pickle
+
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+import pandas as pd
+
+class ClubURLsScraper(AbstractScraper):
+    def __init__(self) -> None:
+        print("Club URL scraper")
+        self.urls = []
+        self.html = None
+        self.soup = None
+        self.result = None
+        self.max_workers = 3
+        self.counter = 0
+
+    def run_crawler(self):
+        self.get_soup(self.urls)
+        self.extract_data(self.soup)
+
+    def start_crawl_threads(self, urls):        
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor: 
+            executor.map(self.run_crawler, self.urls)
+
+    def get_soup(self, url):
+        self.html = requests.get(url).text
+        self.soup = BeautifulSoup(self.html, 'html.parser')
+        
+    def extract_data(self, soup):
+        table = soup.find('tbody')
+        links = table.find_all('a')
+        result = ["https://fbref.com" + link['href'] for link in links if 'squads' in link['href']]
+    
+    def show_result(self, result):
+        print(result)
+
+    def save_result(self,result, file_name):
+        pickle.dump(result,open(file_name,'wb'))
