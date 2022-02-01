60f02ae2f1b2d400aaf7689ea63079af9d41c7bc
Kaiomurz Motawara <kaiomurz@gmail.com>
2022-02-01 21:15:47 +0000

    complete all functionality prior to creating ESPNScraper class

--- utility_code/scraper_sandbox.py
+++ utility_code/scraper_sandbox.py
@@ -1,33 +1,56 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
-playwright = sync_playwright().start()
-browser = playwright.chromium.launch(headless=False)
-page = browser.new_page()
-page.goto("https://www.espn.co.uk/football/")
 
-page.locator("text=Continue without Accepting").click()
-page.locator("id=global-search-trigger").click()
-page.locator("id=global-search-input").fill("Lionel Messi")
-# # page.wait_for_timeout(10000)
-page.locator("//html/body/div[5]/div[2]/header/div[2]/ul/li[1]/div/div[1]/input[2]").click()
+test_info_dict = {
+    1:{"name": "Lionel Messi"}, 
+    2:{"name": "Fabinho"},
+    3:{"name": "Thomas Müller"},
+    4:{"name": "Error Test"},
+    5:{"name": "Josip Stanišić"}
+}
 
-page.wait_for_timeout(5000)
-html = page.content()
-soup = BeautifulSoup(html, 'html.parser')
+news_dict = {}
 
-news_divs = soup.find_all("li", class_="article__Results__Item")
-news_list = []
-for div in news_divs:
-    text = div.text
-    ellipsis_loc = text.find("…")
-    text = text[:ellipsis_loc]
-    news_list.append((text,div.find('a')['href']))
-    # print(div.text)
-    link = div.find('a')
-    # print(link['href'])
+def get_news_list(soup):
+    news_divs = soup.find_all("li", class_="article__Results__Item")
+    news_list = []
+    for div in news_divs:
+        text = div.text
+        ellipsis_loc = text.find("…")
+        text = text[:ellipsis_loc]
+        news_list.append((text,div.find('a')['href']))
+        # print(div.text)
+        link = div.find('a')
+        # print(link['href'])
+    return news_list
 
-print(news_list)
+for key in test_info_dict:
+    name = test_info_dict[key]["name"]
+    print(name)
+    url = "https://www.espn.co.uk/football/"
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto(url)
 
-browser.close()
-playwright.stop()
+    page.locator("text=Continue without Accepting").click()
+    page.locator("id=global-search-trigger").click()
+    page.locator("id=global-search-input").fill(name)
+    print("name filled")
+    # # page.wait_for_timeout(10000)
+    page.locator("//html/body/div[5]/div[2]/header/div[2]/ul/li[1]/div/div[1]/input[2]").click()
+
+    page.wait_for_timeout(1000)
+    html = page.content()
+
+    browser.close()
+    playwright.stop()
+    soup = BeautifulSoup(html, 'html.parser')
+
+
+    news_dict[key] = get_news_list(soup)
+
+print(news_dict)
+
+
