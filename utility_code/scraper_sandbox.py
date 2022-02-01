0e37a7c8a0a5633bc4b20c74d9dd99753087cacf
Kaiomurz Motawara <kaiomurz@gmail.com>
2022-02-01 20:46:46 +0000

    complete logic of ESPN scrape.

--- utility_code/scraper_sandbox.py
+++ utility_code/scraper_sandbox.py
@@ -1,17 +1,33 @@
-# import asyncio
-# from playwright.async_api import async_playwright
-# from playwright.async_api import async_playwright
-# playwright = await async_playwright().start()
-
-# browser = await playwright.chromium.launch()
-# page = await browser.new_page()
-# await page.goto("http://whatsmyuseragent.org/")
-
 from playwright.sync_api import sync_playwright
-from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
 
 playwright = sync_playwright().start()
-
-browser = playwright.chromium.launch()
+browser = playwright.chromium.launch(headless=False)
 page = browser.new_page()
-page.goto("http://whatsmyuseragent.org/")
+page.goto("https://www.espn.co.uk/football/")
+
+page.locator("text=Continue without Accepting").click()
+page.locator("id=global-search-trigger").click()
+page.locator("id=global-search-input").fill("Lionel Messi")
+# # page.wait_for_timeout(10000)
+page.locator("//html/body/div[5]/div[2]/header/div[2]/ul/li[1]/div/div[1]/input[2]").click()
+
+page.wait_for_timeout(5000)
+html = page.content()
+soup = BeautifulSoup(html, 'html.parser')
+
+news_divs = soup.find_all("li", class_="article__Results__Item")
+news_list = []
+for div in news_divs:
+    text = div.text
+    ellipsis_loc = text.find("…")
+    text = text[:ellipsis_loc]
+    news_list.append((text,div.find('a')['href']))
+    # print(div.text)
+    link = div.find('a')
+    # print(link['href'])
+
+print(news_list)
+
+browser.close()
+playwright.stop()
